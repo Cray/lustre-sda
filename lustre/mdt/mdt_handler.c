f4c94d5d856d966a08d8a3919636f73f849790ad
Vitaly Fertman <vitaly.fertman@seagate.com>
2015-10-20 20:03:21 +0300

    MRP-3042 mdt: intent resend finds not existing locked obj
    a race between intent and unlink results in working on orphan object,
    the intent resend finds the lock and !existent object and LBUG:
        ASSERTION( ((o)->lo_header->loh_attr & LOHA_EXISTS) != 0 )
    in mdt_getattr_name_lock()->lu_object_attr()
    
    add a check for !existent object for RESENT case.
    
    Signed-off-by: Vitaly Fertman <vitaly.fertman@seagate.com>
    Change-Id: I5fd53984ef8ca86f83a1e3174435bc450cc165c4
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/8847
    Tested-by: Jenkins
    Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>
    Reviewed-by: Andriy Skulysh <andriy.skulysh@seagate.com>
    Reviewed-by: Alexey Leonidovich Lyashkov <alexey.lyashkov@seagate.com>

--- lustre/mdt/mdt_handler.c
+++ lustre/mdt/mdt_handler.c
@@ -1504,6 +1504,13 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 		GOTO(out_parent, rc = PTR_ERR(child));
 
 	OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_RESEND, obd_timeout*2);
+	if (!mdt_object_exists(child)) {
+		LU_OBJECT_DEBUG(D_INODE, info->mti_env,
+				&child->mot_obj,
+				"Object doesn't exist!\n");
+		GOTO(out_child, rc = -ENOENT);
+	}
+
 	rc = mdt_check_resent_lock(info, child, lhc);
 	if (rc < 0) {
 		GOTO(out_child, rc);
@@ -1511,13 +1518,6 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
                 mdt_lock_handle_init(lhc);
 		mdt_lock_reg_init(lhc, LCK_PR);
 		try_layout = false;
-
-		if (!mdt_object_exists(child)) {
-			LU_OBJECT_DEBUG(D_INODE, info->mti_env,
-					&child->mot_obj,
-					"Object doesn't exist!\n");
-			GOTO(out_child, rc = -ENOENT);
-		}
 
 		if (!(child_bits & MDS_INODELOCK_UPDATE) &&
 		      mdt_object_exists(child) && !mdt_object_remote(child)) {
