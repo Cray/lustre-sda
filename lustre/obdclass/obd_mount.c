6f9ee2af749458bfe5b9c0ffd01d30018f8cd314
Andrew Perepechko <andrew.perepechko@seagate.com>
2015-12-08 21:02:16 +0300

    MRP-3202: mgc obd device is not cleaned up
    We should always call lustre_stop_mgc()
    even if lustre_start_mgc() fails, since
    otherwise the mgc device won't be cleaned up.
    
    Change-Id: I0c18467580b558ff97e55ddcfabb93f6adae9f78
    Signed-off-by: Andrew Perepechko <andrew.perepechko@seagate.com>

--- lustre/obdclass/obd_mount.c
+++ lustre/obdclass/obd_mount.c
@@ -1296,6 +1296,7 @@ int lustre_fill_super(struct super_block *sb, void *data, int silent)
 				CERROR("Failed to start mgc while mounting "
 					"device %s: rc = %d", lmd->lmd_dev, rc);
                                 lustre_put_lsi(sb);
+				lustre_common_put_super(sb);
                                 GOTO(out, rc);
                         }
                         /* Connect and start */
